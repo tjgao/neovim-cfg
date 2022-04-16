f3d24be5a4b522745da3ce5263c08788edfb68ec
Tiejun Gao <tiejun.gao@dolby.com>
2022-04-17 01:26:04 +1000

    documentation field deprecated

--- lua/nvim-cmp-config/init.lua
+++ lua/nvim-cmp-config/init.lua
@@ -123,9 +123,6 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = false,
   },
-  documentation = {
-      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  },
   experimental = {
       ghost_text = true,
       native_menu = false,
