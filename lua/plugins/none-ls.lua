e15a69145487c386ccc71f8e40f7883535bcb42e
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-02-28 20:58:28 +1100

    none-ls will remove jq soon

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,6 @@ return {
                 null_ls.builtins.formatting.gofumpt,
                 null_ls.builtins.formatting.goimports,
                 null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.jq,
 			},
 		})
 
