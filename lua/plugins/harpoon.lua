0bb2235d09d77587681cb86525288a08e449a2e1
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-02-29 18:27:28 +1100

    Added harpoon.nvim
    removed deprecated commands

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,38 @@
+return {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local harpoon = require("harpoon")
+        harpoon.setup({
+            settings = {
+                save_on_toggle = true,
+                sync_on_ui_close = true,
+            },
+        })
+        vim.keymap.set("n", "<leader>ha", function()
+            harpoon:list():append()
+        end)
+        vim.keymap.set("n", "<leader>hm", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end)
+        vim.keymap.set("n", "<C-1>", function()
+            harpoon:list():select(1)
+        end)
+        vim.keymap.set("n", "<C-2>", function()
+            harpoon:list():select(2)
+        end)
+        vim.keymap.set("n", "<C-3>", function()
+            harpoon:list():select(3)
+        end)
+        vim.keymap.set("n", "<C-4>", function()
+            harpoon:list():select(4)
+        end)
+        vim.keymap.set("n", "<A-j>", function()
+            harpoon:list():prev()
+        end)
+        vim.keymap.set("n", "<A-k>", function()
+            harpoon:list():next()
+        end)
+    end,
+}
