9b150b49ed85c40514aa148677c297935d5600a2
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-02-29 18:38:07 +1100

    Keybinding change for harpoon

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
-    'nvim-treesitter/nvim-treesitter', 
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        local lspconfig = require('nvim-treesitter.configs')
+        local lspconfig = require("nvim-treesitter.configs")
         lspconfig.setup({
-            ensure_installed = {"markdown", "html", "javascript", "bash", "python", "cpp", "go", "lua"},
+            ensure_installed = { "markdown", "html", "javascript", "bash", "python", "cpp", "go", "lua" },
             hightlight = { enable = true },
             indent = { enable = true },
         })
-    end
+    end,
 }
