818f94f1642568ead6cfda5f23cf079d8d3aca42
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-03-03 00:39:49 +1100

    Solve the issue that help doc drives treesitter to crazy

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local lspconfig = require("nvim-treesitter.configs")
         lspconfig.setup({
-            ensure_installed = { "markdown", "html", "javascript", "bash", "python", "cpp", "go", "lua" },
+            ensure_installed = { "markdown", "html", "javascript", "bash", "python", "cpp", "go", "lua", "vimdoc" },
             hightlight = { enable = true },
             indent = { enable = true },
         })
