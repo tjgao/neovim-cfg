1f1dc2ee7610f5b55dae3bb50b4cce49b0d99b5c
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-03-04 22:55:06 +1100

    A keybinding to remove hlsearch

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -75,6 +75,8 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
+keymap("n", "mm", ":noh<CR>", opts)
+
 -- Terminal --
 -- Better terminal navigation
 -- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
