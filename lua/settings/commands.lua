3a8dc180bf3a92cac80c7e72729cb4833f05bcf7
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-03-07 11:54:56 +1100

    Allow force close in buffer-close-all function & check unsaved modified for normal close

--- lua/settings/commands.lua
+++ lua/settings/commands.lua
@@ -1,39 +1,41 @@
-
-vim.opt['background'] = 'dark'
-vim.cmd "set whichwrap+=<,>,[,],h,l"
-vim.cmd [[set iskeyword+=-]]
-vim.cmd [[set laststatus=3]]
+vim.opt["background"] = "dark"
+vim.cmd("set whichwrap+=<,>,[,],h,l")
+vim.cmd([[set iskeyword+=-]])
+vim.cmd([[set laststatus=3]])
 
 -- Do not want to see filler char in diff view
-vim.cmd [[set fillchars+=diff:\ ]]
+vim.cmd([[set fillchars+=diff:\ ]])
 
-vim.cmd('colorscheme nightfox')
+vim.cmd("colorscheme nightfox")
 
 -- Steal from TJ's kickstart.nvim
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- We have new global status line so we want to disable bg color for window separator
 -- also, pick a nicer fg color
-vim.cmd "highlight WinSeparator guibg=none guifg=#4C566A"
-
+vim.cmd("highlight WinSeparator guibg=none guifg=#4C566A")
 
 -- Close all buffers except current
-local function close_allbuf_except_current()
+local function close_allbuf_except_current(opts)
     local cur = vim.api.nvim_win_get_buf(0)
     for _, o in pairs(vim.api.nvim_list_bufs()) do
+        if not opts.bang and vim.fn.getbufinfo(o)[1].changed == 1 then
+            goto continue
+        end
         -- We want to exclude nvim-tree and terminals
         local bt = vim.api.nvim_get_option_value("buftype", { buf = o })
         local ft = vim.api.nvim_get_option_value("filetype", { buf = o })
         if o ~= cur and bt ~= "terminal" and ft ~= "NvimTree" then
-            vim.api.nvim_buf_delete(o, {})
+            vim.api.nvim_buf_delete(o, { force = opts.bang })
         end
+        ::continue::
     end
 end
 
-vim.api.nvim_create_user_command('Bd', close_allbuf_except_current, {})
+vim.api.nvim_create_user_command("Bd", close_allbuf_except_current, { desc = "Close all buffers except current", bang = true })
