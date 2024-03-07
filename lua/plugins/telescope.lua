2f0887e72054f0c64498f85d45a123483495527e
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-03-08 10:49:46 +1100

    Optimisation for keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -125,13 +125,11 @@ M = {
             end, { desc = "Search neovim files" })
             keymap("n", "<leader><leader>", builtin.builtin, { desc = "Search in Telescope" })
             keymap("n", "<leader>sr", builtin.resume, { desc = "Search resume" })
-
-            -- Slightly advanced example of overriding default behavior and theme
             keymap("n", "<leader>/", function()
                 -- You can pass additional configuration to telescope to change theme, layout, etc.
                 builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
                     winblend = 10,
-                    previewer = false,
+                    -- previewer = false,
                 }))
             end, { desc = "Fuzzy search in current buffer" })
         end,
--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -1,12 +1,11 @@
-local opts = { noremap = true, silent = true }
-
--- local term_opts = { silent = true }
-
--- Shorten function name
-local keymap = vim.keymap.set
+local keymap = function(mode, key, action, desc)
+    local opts = { noremap = true, silent = true }
+    opts.desc = desc
+    vim.keymap.set(mode, key, action, opts)
+end
 
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+keymap("", "<Space>", "<Nop>", "Leader remap")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -20,23 +19,23 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h", "Move left to next window")
+keymap("n", "<C-j>", "<C-w>j", "Move down to next window")
+keymap("n", "<C-k>", "<C-w>k", "Move up to next window")
+keymap("n", "<C-l>", "<C-w>l", "Move right to next window")
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Up>", ":resize +2<CR>", "Resize window upward")
+keymap("n", "<C-Down>", ":resize -2<CR>", "Resize window downward")
+keymap("n", "<C-Left>", ":vertical resize +2<CR>", "Resize window leftward")
+keymap("n", "<C-Right>", ":vertical resize -2<CR>", "Resize window rightward")
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", "Go to next buf")
+keymap("n", "<S-h>", ":bprevious<CR>", "Go to previous buf")
 
 -- Retrieve last thing yanked instead of deleted
-keymap("n", "<A-p>", "\"0p", opts)
+keymap("n", "<A-p>", "\"0p", "Past last yanked instead of deleted")
 
 -- Toggle quickfix window
 -- keymap("n", "<C-q>", ":lua require('commands').ToggleQF()<CR>", opts)
@@ -48,7 +47,7 @@ keymap("n", "<A-p>", "\"0p", opts)
 -- keymap("n", "<A-a>", ":lua require('symbols-outline').toggle_outline()<CR>", opts)
 
 -- nvim-tree
-keymap("n", "<F1>", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<F1>", ":NvimTreeToggle<CR>", "Toggle NvimTree")
 
 -- Move text up and down
 -- keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
@@ -56,12 +55,14 @@ keymap("n", "<F1>", ":NvimTreeToggle<CR>", opts)
 
 -- Insert --
 -- Press jk fast to enter
-keymap("i", "jk", "<ESC>", opts)
+keymap("i", "jk", "<ESC>", "Alternative for ESC in insert mode")
 
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap("v", "<", "<gv", "Move visual selected left")
+keymap("v", ">", ">gv", "Move visual selected right")
+keymap("v", "<S-h>", "<gv", "Move visual selected left")
+keymap("v", "<S-l>", ">gv", "Move visual selected right")
 
 -- Move text up and down
 -- keymap("v", "<A-j>", ":m .+1<CR>==", opts)
@@ -70,12 +71,12 @@ keymap("v", ">", ">gv", opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", "Move visual selected down")
+keymap("x", "K", ":move '<-2<CR>gv-gv", "Move visual selected up")
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", "Move visual selected down")
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", "Move visual selected up")
 
-keymap("n", "mm", ":noh<CR>", opts)
+keymap("n", "mm", ":noh<CR>", "Clear search highlight")
 
 -- Terminal --
 -- Better terminal navigation
