965d500adc4babbd896c81034a20f8a9bb6b3616
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-03-14 01:54:44 +1100

    Added indent plugin

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,14 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+        indent = {
+            char = "",
+            -- tab_char = "󰌒",
+            -- tab_char = ".",
+        },
+        scope = {
+            enabled = false,
+        },
+    },
+}
