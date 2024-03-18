2712abb3d1c45e010112d17e8c908e90a08b7d16
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-03-18 23:42:44 +1100

    Exclude harpoon buffers when calling :Bd command

--- lua/users/buf_close.lua
+++ lua/users/buf_close.lua
@@ -4,9 +4,26 @@
 -- as well. Nvim-tree and terminal buffers are untouched, because that's
 -- my preference, I'd like to have them around.
 --
--- Close all buffers except current
+-- Close all buffers except current and harpoon bufs (if excluded)
+
+vim.g.bd_exclude_harpoon = true
+
+local function delete_buf(cwd, items, buf, opts)
+    if vim.g.bd_exclude_harpoon then
+        for _, v in ipairs(items) do
+            if cwd .. "/" .. v.value == vim.fn.expand("#" .. buf .. ":p") then
+                return
+            end
+        end
+    end
+    vim.api.nvim_buf_delete(buf, { force = opts.bang })
+end
+
 local function close_allbuf_except_current(opts)
     local cur = vim.api.nvim_win_get_buf(0)
+    local status, harpoon = pcall(require, "harpoon")
+    local items = (status and harpoon:list().items) or {}
+    local cwd = vim.fn.getcwd()
     for _, o in pairs(vim.api.nvim_list_bufs()) do
         if not opts.bang and vim.fn.getbufinfo(o)[1].changed == 1 then
             goto continue
@@ -15,7 +32,7 @@ local function close_allbuf_except_current(opts)
         local bt = vim.api.nvim_get_option_value("buftype", { buf = o })
         local ft = vim.api.nvim_get_option_value("filetype", { buf = o })
         if o ~= cur and bt ~= "terminal" and ft ~= "NvimTree" then
-            vim.api.nvim_buf_delete(o, { force = opts.bang })
+            delete_buf(cwd, items, o, opts)
         end
         ::continue::
     end
