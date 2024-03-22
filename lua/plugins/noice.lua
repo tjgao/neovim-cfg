72878f427d8fd7d0bec88c9a5c00807b0210f73c
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-03-23 02:14:14 +1100

    Customize noice cmdline popup postion & colorscheme -> catppuccin

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -13,6 +13,11 @@ return {
         "rcarriga/nvim-notify",
     },
     config = function()
+        -- Put cmdline popup on the screen around 38% of the height
+        -- popup menu shows up 3 rows lower
+        local lines = vim.fn.winheight(0)
+        local row = math.floor(lines * (1 - 0.618))
+        local pop_row = row + 3
         require("noice").setup({
             lsp = {
                 -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
@@ -25,22 +30,38 @@ return {
             -- you can enable a preset for easier configuration
             presets = {
                 -- bottom_search = true, -- use a classic bottom cmdline for search
-                command_palette = true,       -- position the cmdline and popupmenu together
+                -- command_palette = true,       -- position the cmdline and popupmenu together
                 long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = false,       -- add a border to hover docs and signature help
+                inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false, -- add a border to hover docs and signature help
             },
             views = {
                 cmdline_popup = {
                     position = {
-                        row = "30%",
+                        row = row,
                         col = "50%",
+                    },
+                    size = {
+                        width = 60,
+                        height = "auto",
                     },
                 },
                 popupmenu = {
+                    relative = "editor",
                     position = {
-                        row = "33%",
+                        row = pop_row,
                         col = "50%",
+                    },
+                    size = {
+                        width = 60,
+                        height = 10,
+                    },
+                    border = {
+                        style = "rounded",
+                        padding = { 0, 1 },
+                    },
+                    win_options = {
+                        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
                     },
                 },
             },
