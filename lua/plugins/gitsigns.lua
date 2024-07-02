351dcf2c8e84a163a35a1af44ad51c174fa456e6
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-07-02 17:25:44 +1000

    Fix warnings caused by updates

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -14,25 +14,25 @@ return {
             --    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
             --  },
             signs = {
-                add = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-                change = { hl = "GitSignsChange", text = "^", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-                delete = { hl = "GitSignsDelete", text = "-", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+                add = {
+                    text = "+",
+                },
+                change = {
+                    text = "^",
+                },
+                delete = {
+                    text = "-",
+                },
                 topdelete = {
-                    hl = "GitSignsDelete",
                     text = "‾",
-                    numhl = "GitSignsDeleteNr",
-                    linehl = "GitSignsDeleteLn",
                 },
                 changedelete = {
-                    hl = "GitSignsChange",
                     text = "~",
-                    numhl = "GitSignsChangeNr",
-                    linehl = "GitSignsChangeLn",
                 },
             },
             signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-            numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
-            linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
+            numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+            linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
             word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
             watch_gitdir = {
                 interval = 1000,
