2175858066623a47ec96e7982aa97ee586f6f078
Tiejun Gao <terry.tiejun.gao@gmail.com>
2025-11-08 21:39:21 +1100

    Added treesj & picker improvement

--- after/plugin/settings.lua
+++ after/plugin/settings.lua
@@ -1,3 +1,42 @@
+-- This code is to make normal and insert mode more distinguishable in snacks picker
+-- Because I prefer use j/k to move down/up in normal mode
+-- Currently I find this code works well for all except keymaps search, meaning the
+-- prompt color is changed back to default in insert mode now and then
+-- I suspect Snacks does something special in Snacks.picker.keymaps()
+local original_prompt = vim.api.nvim_get_hl(0, { name = "SnacksPickerPrompt" })
+
+local picker_highlights = {
+    normal = {
+        SnacksPickerPrompt = {
+            fg = original_prompt.fg,
+            bold = true,
+            force = true,
+        },
+    },
+    insert = {
+        SnacksPickerPrompt = {
+            fg = "#aa5533",
+            bold = true,
+            force = true,
+        },
+    },
+}
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+    callback = function()
+        if vim.bo.filetype:find("^snacks_picker_") == nil then
+            return
+        end
+
+        local mode = vim.fn.mode()
+        local hl = (mode == "i") and picker_highlights.insert or picker_highlights.normal
+
+        for group, opts in pairs(hl) do
+            vim.api.nvim_set_hl(0, group, opts)
+        end
+    end,
+})
+
 -- This is to change the color of the indent scope
 vim.api.nvim_create_autocmd("ColorScheme", {
     pattern = "*",
