011ce9e4a98846f496d0099573483085f7426b97
Tiejun Gao <terry.tiejun.gao@gmail.com>
2025-11-10 23:44:11 +1100

    Added an autocmd to move to the last location in buffer; more langs for ts

--- lua/settings/commands.lua
+++ lua/settings/commands.lua
@@ -17,6 +17,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     end,
 })
 
+vim.api.nvim_create_autocmd("BufReadPost", {
+    desc = "Go to the last location when openning a buffer",
+    group = vim.api.nvim_create_augroup("last_location", { clear = true }),
+    callback = function(args)
+        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+        local line_count = vim.api.nvim_buf_line_count(args.buf)
+        if mark[1] > 0 and mark[1] <= line_count then
+            vim.cmd('normal! g`"zz')
+        end
+    end,
+})
+
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "qf",
     group = vim.api.nvim_create_augroup("quickfix", { clear = true }),
