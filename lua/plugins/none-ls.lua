e7112490bc3a9600ae067bd05caccc447e388008
Tiejun Gao <tgao@emvision.com.au>
2026-03-10 10:26:58 +1100

    Adjust c/c++ format and autoformat

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,7 +10,8 @@
 --     end,
 -- })
 
-local autofmt_patterns = { "*.go", "*.lua", "*.json", "*.py", "*.js", "*.ts", "*.tsx", "*.jsx", "*.html" }
+local autofmt_patterns =
+    { "*.go", "*.lua", "*.json", "*.py", "*.js", "*.ts", "*.tsx", "*.jsx", "*.html", "*.cpp", "*.h" }
 local group_name = "AutoFormatGroup_None-ls"
 local install_autofmt = function(patterns)
     local group = vim.api.nvim_create_augroup(group_name, {})
