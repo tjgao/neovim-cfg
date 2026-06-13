7355626b25aa62917f9f636936032c6c8c882ac5
Tiejun Gao <terry.tiejun.gao@gmail.com>
2026-06-14 01:57:26 +1000

    Minor fix: QuickBuf config

--- lua/plugins/quickbuf.lua
+++ lua/plugins/quickbuf.lua
@@ -4,7 +4,6 @@ return {
     "tjgao/quickbuf.nvim",
     config = function()
         require("quickbuf").setup({
-            max_items = 20, -- height cap (line count)
             window = {
                 min_width = 0.3,
                 max_width = 0.7,
@@ -13,8 +12,4 @@ return {
             },
         })
     end,
-    vim.keymap.set("n", "<Tab>", ":QuickBuf<CR>", { desc = "QuickBuf" }),
-    vim.keymap.set("n", "<leader>qt", ":QuickBufPinToggle<CR>", { desc = "QuickBufPinToggle" }),
-    vim.keymap.set("n", "<S-h>", ":QuickBufPrevPinned<CR>", { desc = "Prev pinned buffer" }),
-    vim.keymap.set("n", "<S-l>", ":QuickBufNextPinned<CR>", { desc = "Next pinned buffer" }),
 }
