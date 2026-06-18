598f28b84950244957a60e260aae7f613665ca2c
Tiejun Gao <terry.tiejun.gao@gmail.com>
2026-06-19 00:36:04 +1000

    Turn on quickbuf persistence

--- lua/plugins/quickbuf.lua
+++ lua/plugins/quickbuf.lua
@@ -4,11 +4,8 @@ return {
     "tjgao/quickbuf.nvim",
     config = function()
         require("quickbuf").setup({
-            window = {
-                min_width = 0.3,
-                max_width = 0.7,
-                border = "rounded",
-                padding = 2,
+            persistence = {
+                enabled = true,
             },
         })
     end,
